bb8af29450e5aec0bc53f41497d210069051b4e2
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-06-16 19:59:42 -0500

    Tiggers
    Tiggers.sql

---
+++ Tiggers.sql
@@ -0,0 +1,17 @@
+-- 1. Insert detalle pedido 
+
+DELIMITER $$
+
+CREATE TRIGGER tg_before_insert_detalle_pedido
+BEFORE INSERT ON detalle_pedido
+FOR EACH ROW
+BEGIN
+    IF NEW.cantidad < 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La cantidad debe ser al menos 1';
+    END IF;
+END $$
+
+DELIMITER ;
+
+INSERT INTO detalle_pedido (pedido_id, cantidad)VALUES (1, 0);
